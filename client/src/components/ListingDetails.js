20a49a97f64328268a58ee0368efd66144172dc3
Jess Hilliam <JessHilliam@Jesss-Air.hitronhub.home>
2020-11-12 11:06:30 -0800

    basic setup of ResultsPage and ListingDetails components

---
+++ client/src/components/ListingDetails.js
@@ -0,0 +1,7 @@
+export default function ListingDetails() {
+  return (
+    <section className="listing-deets">
+      <h1>Test</h1>
+    </section>
+  )
+}
---
+++ client/src/components/ResultsPage.js
@@ -0,0 +1,12 @@
+import ListingDetails from "./ListingDetails";
+
+export default function ResultsPage() {
+  return (
+    <main className="results-page">
+      {/* search bar & filters? */}
+      {/* results lists on the left */}
+      {/* specific listing details on the right (sticky) */}
+      <ListingDetails />
+    </main>
+  )
+}
